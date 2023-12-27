12d3b9989dd3556fd24e820d6a7a6708fa9b2b7c
Emiliano Alejandro <alejandroemiliano21@gmail.com>
2023-12-27 12:25:04 -0300

    NavBar parte 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 
-import CardContent from './components/card/cardContent/CardContent'
+// import CardContent from './components/card/cardContent/CardContent'
+import NavBar from './components/nav/NavBar'
 import './index.css'
+
 
 function App() {
 
   return (
-    <div className='bg-gray-300 w-full h-full'>
-     <CardContent /> 
+    <div className='bg-[#E4F7F8] w-full h-screen'>
+     <NavBar />
     </div>
   )
 }
